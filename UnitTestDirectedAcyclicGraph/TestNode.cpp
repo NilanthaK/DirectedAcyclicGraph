6d238b1d14252b4ed0a149adb5f8b2cf126da8cf
Katugampala <nkatugam@mgc.mentorg.com>
2017-06-30 01:10:49 +0100

    Update TestNode

--- UnitTestDirectedAcyclicGraph/TestNode.cpp
+++ UnitTestDirectedAcyclicGraph/TestNode.cpp
@@ -1,8 +1,9 @@
 #include "CppUnitTest.h"
-
-#include "Node.h"
+#include "Node.hpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+using namespace dag;
 
 namespace UnitTestDirectedAcyclicGraph
 {		
@@ -10,16 +11,24 @@ namespace UnitTestDirectedAcyclicGraph
 	{
 	public:
 		
-		TEST_METHOD(LinkNewNextNode)
+		TEST_METHOD(NextNodes)
 		{
-			vector<Node*> empty_next_nodes;
-			Node node1("Add Reagent 1", empty_next_nodes);
-			Node node2("Add Reagent 2", empty_next_nodes);
+			const string AddReagent1 = "Add Reagent 1";
+			const string AddReagent2 = "Add Reagent 2";
 
-			node1.LinkNewNextNode(&node2);
-			vector<Node*> next_nodes = node1.NextNodes();
+			vector<Node<int>*> next_nodes;
+			Node<int> node2(2, AddReagent2, next_nodes);
 
+			next_nodes.push_back(&node2);
+			Node<int> node1(1, AddReagent1, next_nodes);
+
+			next_nodes = node1.NextNodes();
+
+			Assert::IsTrue(1 == next_nodes.size());
 			Assert::IsTrue(&node2 == next_nodes[0]);
+
+			Assert::IsTrue(AddReagent1 == node1.Name());
+			Assert::IsTrue(1 == node1.PayLoad());
 		}
 
 	};
